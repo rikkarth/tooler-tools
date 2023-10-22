3ac06190b2324d6a06d69aaacbe6648dba64d19c
rikkarth <ricardominamendes@gmail.com>
2023-10-22 23:59:59 +0100

    SystemEnvParser Initial Implementation

---
+++ src/main/java/org/toolertools/pathfinder/SystemEnvParser.java
@@ -0,0 +1,48 @@
+package org.toolertools.pathfinder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SystemEnvParser {
+
+    private SystemEnvParser() {
+        throw new AssertionError("SystemEnvParser should not be instantiated.");
+    }
+
+    public static String getEnvPath(String input) {
+        AtomicReference<String> inputCopy = new AtomicReference<>(input);
+        List<String> pathComponentsList = Arrays.asList((inputCopy.get()).split("[/\\\\:-]"));
+        parseIfMys(inputCopy, pathComponentsList);
+        parseIfUnix(inputCopy, pathComponentsList);
+        return inputCopy.get();
+    }
+
+    private static void parseIfMys(AtomicReference<String> inputCopy,
+        List<String> pathComponentsList) {
+        pathComponentsList
+            .stream()
+            .filter(component -> component.startsWith("%") && component.endsWith("%"))
+            .forEach(component -> {
+                String envVarName = component.substring(1, component.length() - 1);
+                String envVarValue = System.getenv(envVarName);
+                if (envVarValue != null) {
+                    inputCopy.set((inputCopy.get()).replace(component, envVarValue));
+                }
+            });
+    }
+
+    private static void parseIfUnix(AtomicReference<String> inputCopy,
+        List<String> pathComponentsList) {
+        pathComponentsList
+            .stream()
+            .filter(component -> component.startsWith("$"))
+            .forEach(component -> {
+                String envVarName = component.substring(1);
+                String envVarValue = System.getenv(envVarName);
+                if (envVarValue != null) {
+                    inputCopy.set((inputCopy.get()).replace(component, envVarValue));
+                }
+            });
+    }
+}

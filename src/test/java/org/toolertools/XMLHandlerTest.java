841f3ad498083a71e0c2b3c1df2eff203b885e31
rikkarth <ricardominamendes@gmail.com>
2023-10-22 01:55:54 +0100

    More XMLHandler Unit Tests Created

--- src/test/java/org/toolertools/XMLHandlerTest.java
+++ src/test/java/org/toolertools/XMLHandlerTest.java
@@ -1,6 +1,5 @@
 package org.toolertools;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
 
@@ -9,6 +8,8 @@ import javax.xml.xpath.XPathExpressionException;
 import java.io.File;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class XMLHandlerTest {
 
     @Test
@@ -16,12 +17,17 @@ public class XMLHandlerTest {
         Optional<Document> optionalDocument = XMLHandler.getOptionalDomFromFile(new File("src/test/resources/test_resource_1.xml"));
         optionalDocument.ifPresent(doc -> {
             try {
-                XPathExpression xpeElement1 = XMLHandler.createXPathExpression("/root/element1/text()");
-                XPathExpression xpeEmptyElement = XMLHandler.createXPathExpression("/root/emptyElement/text()");
+                String element1XPath = "/root/element1/text()";
+                String emptyElementXPath = "/root/emptyElement/text()";
+
+                XPathExpression xpeElement1 = XMLHandler.createXPathExpression(element1XPath);
+                XPathExpression xpeEmptyElement = XMLHandler.createXPathExpression(emptyElementXPath);
+
                 String element1 = XMLHandler.getStringFromXPath(xpeElement1, doc);
                 String emptyElement = XMLHandler.getStringFromXPath(xpeEmptyElement, doc);
-                assertFalse(element1.isEmpty(), "Element 1 is not empty");
-                assertTrue(emptyElement.isEmpty(), "emptyElement is empty");
+
+                assertFalse(element1.isEmpty(), element1XPath + " should not be empty");
+                assertTrue(emptyElement.isEmpty(), emptyElementXPath + " should be empty");
             } catch (XPathExpressionException xpee) {
                 throw new RuntimeException(xpee);
             }
@@ -33,14 +39,31 @@ public class XMLHandlerTest {
         Optional<Document> optionalDocument = XMLHandler.getOptionalDomFromFile(new File("src/test/resources/test_resource_1.xml"));
         optionalDocument.ifPresent(doc -> {
             try {
-                XPathExpression xpeElement1 = XMLHandler.createXPathExpression("/root/element1/text()");
-                XPathExpression xpeEmptyElement = XMLHandler.createXPathExpression("/root/emptyElement/text()");
+                String element1XPath = "/root/element1/text()";
+                String emptyElementXPath = "/root/emptyElement/text()";
+                XPathExpression xpeElement1 = XMLHandler.createXPathExpression(element1XPath);
+                XPathExpression xpeEmptyElement = XMLHandler.createXPathExpression(emptyElementXPath);
                 String element1 = XMLHandler.getStringFromXPath(xpeElement1, null);
                 String emptyElement = XMLHandler.getStringFromXPath(xpeEmptyElement, null);
-                assertTrue(element1.isEmpty(), "Element 1 is not empty");
-                assertTrue(emptyElement.isEmpty(), "emptyElement is empty");
+                assertTrue(element1.isEmpty(), element1XPath + " should not be empty");
+                assertTrue(emptyElement.isEmpty(), emptyElementXPath + "should be empty");
             } catch (XPathExpressionException xpee) {
                 throw new RuntimeException(xpee);
+            }
+        });
+    }
+
+    @Test
+    public void testCreateXPathExpression() {
+        Optional<Document> optionalDocument = XMLHandler.getOptionalDomFromFile(new File("src/test/resources/test_resource_1.xml"));
+        optionalDocument.ifPresent(doc -> {
+            try {
+                String malformedXPath = "/root/element1/tet()";
+                String compliantXPath = "/root/emptyElement/text()";
+                assertDoesNotThrow(() -> XMLHandler.createXPathExpression(compliantXPath), "should not throw");
+                assertThrows(XPathExpressionException.class, () -> XMLHandler.createXPathExpression(malformedXPath), "should throw " + XPathExpressionException.class);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
         });
     }

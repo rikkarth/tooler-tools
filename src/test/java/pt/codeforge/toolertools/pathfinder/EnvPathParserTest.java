8a3a0975a26659c5841988cdcc970b69046e6219
rikkarth <ricardo.mendes@codeforge.pt>
2023-12-05 23:00:49 +0000

    Minor Test Fix

--- src/test/java/pt/codeforge/toolertools/pathfinder/EnvPathParserTest.java
+++ src/test/java/pt/codeforge/toolertools/pathfinder/EnvPathParserTest.java
@@ -1,5 +1,6 @@
 package pt.codeforge.toolertools.pathfinder;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,20 +9,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 class EnvPathParserTest {
 
     @BeforeEach
-    void setup(){
+    void setup() {
         System.setProperty("$HOME", "test");
     }
 
     @ParameterizedTest
     @ValueSource(strings = "$HOMEDRIVE/$HOMEPATH")
-    void testGetEnvPath(String input){
+    void testGetEnvPath(String input) {
 
         System.out.println(EnvPathParser.getEnvPath(input));
     }
 
     @Test
-    void testBadGetEnvPath(){
-        System.out.println(EnvPathParser.getEnvPath(null));
+    void testBadGetEnvPath() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> EnvPathParser.getEnvPath(null));
     }
 
 }

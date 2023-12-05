5305d1b63aebc40830f567ee82100be8848cc8ac
rikkarth <ricardo.mendes@codeforge.pt>
2023-12-05 22:48:30 +0000

    EnvParser Initial Implementation, JavaDoc and Unit Test Start

--- src/main/java/pt/codeforge/toolertools/pathfinder/EnvPathParser.java
+++ src/main/java/pt/codeforge/toolertools/pathfinder/EnvPathParser.java
@@ -1,21 +1,48 @@
 package pt.codeforge.toolertools.pathfinder;
 
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class SystemEnvParser {
+/**
+ * Utility class for parsing and expanding environment variables in file paths. This class provides static methods for
+ * parsing input paths, replacing environment variable placeholders with their respective values on both Windows and
+ * Unix-like systems.
+ *
+ * <p><b>Note:</b> This class should not be instantiated as it consists of static utility methods only.
+ *
+ * @see EnvPathParser#getEnvPath(String)
+ * @see EnvPathParser#parseIfMys(AtomicReference, List)
+ * @see EnvPathParser#parseIfUnix(AtomicReference, List)
+ */
+public class EnvPathParser {
 
-    private SystemEnvParser() {
-        throw new AssertionError("SystemEnvParser should not be instantiated.");
+    private EnvPathParser() {
+        throw new AssertionError("EnvPathParser should not be instantiated.");
     }
 
+    /**
+     * Parses the input path by replacing environment variable placeholders with their respective values. Environment
+     * variable placeholders are denoted by percent signs ('%') on both ends for Windows and by a dollar sign ('$') at
+     * the beginning for Unix-like systems. The method handles both styles. After parsing, the method returns the
+     * normalized path as a String.
+     *
+     * @param input The input path to be parsed. Must not be null.
+     * @return The parsed and normalized path as a String.
+     * @throws IllegalArgumentException If the input parameter is null.
+     */
     public static String getEnvPath(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("Input cannot be null.");
+        }
+
         AtomicReference<String> inputCopy = new AtomicReference<>(input);
         List<String> pathComponentsList = Arrays.asList((inputCopy.get()).split("[/\\\\:-]"));
         parseIfMys(inputCopy, pathComponentsList);
         parseIfUnix(inputCopy, pathComponentsList);
-        return inputCopy.get();
+
+        return Paths.get(inputCopy.get()).toString();
     }
 
     private static void parseIfMys(AtomicReference<String> inputCopy,
---
+++ src/test/java/pt/codeforge/toolertools/pathfinder/EnvPathParserTest.java
@@ -0,0 +1,27 @@
+package pt.codeforge.toolertools.pathfinder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class EnvPathParserTest {
+
+    @BeforeEach
+    void setup(){
+        System.setProperty("$HOME", "test");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "$HOMEDRIVE/$HOMEPATH")
+    void testGetEnvPath(String input){
+
+        System.out.println(EnvPathParser.getEnvPath(input));
+    }
+
+    @Test
+    void testBadGetEnvPath(){
+        System.out.println(EnvPathParser.getEnvPath(null));
+    }
+
+}

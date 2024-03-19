3f0ded8620e48ff926fb927371d36f48f502c18f
rikkarth <ricardo.mendes@codeforge.pt>
2024-03-19 23:09:53 +0000

    change(ZipBuilder): made ZipBuilderException.java package-private

--- src/main/java/pt/codeforge/toolertools/zip/ZipBuilderException.java
+++ src/main/java/pt/codeforge/toolertools/zip/ZipBuilderException.java
@@ -1,6 +1,6 @@
 package pt.codeforge.toolertools.zip;
 
-public class ZipBuilderException extends RuntimeException {
+class ZipBuilderException extends RuntimeException {
 
     public ZipBuilderException(String message, Throwable cause) {
         super(message, cause);
